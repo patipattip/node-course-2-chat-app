cf0920868bf0aa5da7894ec0b83c02192d13990b
patipat <patipat.tip@gmail.com>
2017-07-01 15:34:21 +0700

    Add Users class and updateUserList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,7 +17,16 @@ function scrollToBottom () {
 }
 
 socket.on('connect', function () {
-  console.log('Connected to server');
+  var params = jQuery.deparam(window.location.search);
+  socket.emit('join', params, function (err) {
+    if (err) {
+      alert(err);
+      window.location.href = '/';
+    } else {
+      console.log('No error');
+    }
+  });
+  // console.log('Connected to server');
 
   // socket.emit('createMessage', {
   //   from: 'Andrew',
@@ -27,6 +36,17 @@ socket.on('connect', function () {
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
+});
+
+socket.on('updateUserList', function (users) {
+  // console.log('Users list', users);
+  var ol = jQuery('<ol></ol>');
+
+  users.forEach(function (user) {
+    ol.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery('#users').html(ol);
 });
 
 socket.on('newMessage', function (message) {
---
+++ server/utils/user.js
@@ -0,0 +1,49 @@
+// var users = [];
+
+// var addUser = (id, name, room) => {
+//   users.push({
+//     id,
+//     name,
+//     room
+//   });
+// };
+
+// modules.export = {addUser};
+
+class Users {
+  constructor() {
+    this.users = [];
+  }
+
+  addUser(id, name, room) {
+    var user = {
+      id,
+      name,
+      room
+    };
+    this.users.push(user);
+    return user;
+  }
+
+  removeUser(id) {
+    var user = this.getUser(id);
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
+    }
+
+    return user;
+  }
+
+  getUser(id) {
+    return this.users.filter((user) => user.id === id)[0];
+  }
+
+  getUserList(room) {
+    var users = this.users.filter((user) => user.room === room);
+    var namesArray = users.map((user) => user.name);
+
+    return namesArray;
+  }
+}
+
+module.exports = { Users };
